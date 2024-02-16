aee28d3dd4e22dfafa4b5c57cc78b4ed8bc0cbaf
Take Khounphaxay <takekhounphaxay@Takes-MacBook-Air.local>
2024-02-16 20:18:03 +0700

    first commit

---
+++ lib/company_info.dart
@@ -0,0 +1,50 @@
+library company_info;
+
+import 'package:flutter/material.dart';
+import 'logo_enum.dart';
+
+// ignore: camel_case_types
+class companyInfo extends StatelessWidget {
+  final LogoType logoType;
+  final double width;
+  final double height;
+  final bool showName;
+  final Color? color;
+  final String? customeName;
+  final TextStyle? nameStyle;
+  const companyInfo({
+    Key? key,
+    required this.logoType,
+    this.width = 100,
+    this.height = 100,
+    this.showName = false,
+    this.nameStyle,
+    this.customeName,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (showName)
+          Text(
+            logoType.name,
+            style: nameStyle ?? Theme.of(context).textTheme.headline6,
+          )
+        else
+          Text(
+            customeName ?? "",
+            style: nameStyle ?? Theme.of(context).textTheme.headline6,
+          ),
+        const SizedBox(width: 5),
+        Image.asset(
+          logoType.asset,
+          width: width,
+          height: height,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/logo_enum.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: no_duplicate_case_values
+
+enum LogoType {
+  bcel,
+  jdb,
+  ldb,
+  maruhan,
+  mixay,
+  ans,
+  hal,
+  acle,
+  ib,
+  mepom,
+  bic,
+  hiapp,
+  icbbc,
+  kbank,
+  lvd,
+  mmoney,
+  vtb,
+}
+
+extension LogoTypeExtension on LogoType {
+  String get name {
+    switch (this) {
+      case LogoType.bcel:
+        return 'BCEL One';
+      case LogoType.jdb:
+        return 'JDB Yes';
+      case LogoType.ldb:
+        return 'LDB Trust';
+      case LogoType.maruhan:
+        return 'Maruhan Japan Bank Lao';
+      case LogoType.mixay:
+        return 'Mixay express';
+      case LogoType.ans:
+        return 'Anousith express';
+      case LogoType.hal:
+        return 'Hal express';
+      case LogoType.acle:
+        return 'ACLEDA Bank';
+      case LogoType.mepom:
+        return 'Mepom';
+      case LogoType.bic:
+        return 'BIC BANK LAO';
+      case LogoType.hiapp:
+        return 'Hi App';
+      case LogoType.icbbc:
+        return 'ICBC';
+      case LogoType.kbank:
+        return 'Kplus Lao';
+      case LogoType.lvd:
+        return 'LVB DigiBank';
+      case LogoType.mmoney:
+        return 'M MoneyX';
+      case LogoType.vtb:
+        return 'VietinBank LAOiPay';
+      case LogoType.ib:
+        return 'IB Cool';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get asset {
+    switch (this) {
+      case LogoType.mixay:
+        return 'assets/logo/express/mixay-logo.png';
+      case LogoType.ans:
+        return 'assets/logo/express/ans-logo.webp';
+      case LogoType.hal:
+        return 'assets/logo/express/hal-logo.png';
+      case LogoType.bcel:
+        return 'assets/logo/bank/bcel.png';
+      case LogoType.jdb:
+        return 'assets/logo/bank/jdb.png';
+      case LogoType.ldb:
+        return 'assets/logo/bank/ldb.png';
+      case LogoType.maruhan:
+        return 'assets/logo/bank/maruhan.png';
+      case LogoType.mepom:
+        return 'assets/logo/bank/mepom.webp';
+      case LogoType.acle:
+        return 'assets/logo/bank/acle.jpeg';
+      case LogoType.icbbc:
+        return 'assets/logo/bank/icbc.jpg';
+      case LogoType.ib:
+        return 'assets/logo/bank/ib.jpg';
+      case LogoType.mmoney:
+        return 'assets/logo/bank/mmoney.png';
+      case LogoType.kbank:
+        return 'assets/logo/bank/kbank-lao.png';
+      case LogoType.lvd:
+        return 'assets/logo/bank/lvb.jpeg';
+      case LogoType.hiapp:
+        return 'assets/logo/bank/hiapp.png';
+      case LogoType.vtb:
+        return 'assets/logo/bank/vtb.webp';
+      case LogoType.bic:
+        return 'assets/logo/bank/bic.jpg';
+      default:
+        return 'assets/logo/bank/bcel.webp';
+    }
+  }
+}
